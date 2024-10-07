a5182d738f742466b1f2043dad0745b32c27a067
Vivek Vibhuti <vivekvibhutiofficial@gmail.com>
2024-10-07 13:40:22 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Ensure this path matches your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
